4d8f07c2089aa3a1cebc53dddc0a385d94900f8f
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2024-01-09 18:22:53 -0600

    documentation added to AppModule

--- app/src/main/java/com/munbonecci/di/AppModule.kt
+++ app/src/main/java/com/munbonecci/di/AppModule.kt
@@ -7,9 +7,18 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
+/**
+ * Dagger Hilt module responsible for providing application-level dependencies.
+ */
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    /**
+     * Provides the application context as a dependency.
+     * @param context The application context provided by Dagger Hilt.
+     * @return The provided application context.
+     */
     @Provides
     fun providesContext(@ApplicationContext context: Context) = context
 }
