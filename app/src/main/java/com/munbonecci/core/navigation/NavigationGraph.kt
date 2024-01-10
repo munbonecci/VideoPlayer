aac7a722c98bd28e34b8d88073308ee7ce6587d2
edmundorobertobonequilevine <edmundo.bonequi@coppel.com>
2024-01-09 18:21:17 -0600

    documentation added to Navigation

--- app/src/main/java/com/munbonecci/core/navigation/NavigationGraph.kt
+++ app/src/main/java/com/munbonecci/core/navigation/NavigationGraph.kt
@@ -10,22 +10,35 @@ import androidx.navigation.navArgument
 import com.munbonecci.videoplayer.features.video.VideoScreen
 import com.munbonecci.videoplayer.features.video_detail.VideoDetailScreen
 
+/**
+ * Composable function representing the navigation graph of the application using Jetpack Compose.
+ * @param navController The NavController responsible for handling navigation between composable.
+ */
 @Composable
 fun NavigationGraph(navController: NavHostController) {
+    // Define the navigation graph using NavHost
     NavHost(navController, startDestination = NavigationItem.Video.route) {
+        // Composable for the main video listing screen
         composable(NavigationItem.Video.route) {
+            // Handle back button press when on the main video screen
             BackHandler(true) {}
+            // Display the main video screen
             VideoScreen(onVideoPressed = {
+                // Navigate to the video detail screen when a video is pressed
                 navController.navigate("${NavigationItem.VideoDetail.route}/${it.videoResultEntity.id}")
             })
         }
+
+        // Composable for the video detail screen with dynamic videoId argument
         composable(
             "${NavigationItem.VideoDetail.route}/{videoId}",
             arguments = listOf(
                 navArgument("videoId") { type = NavType.StringType }
             )
         ) {
+            // Display the video detail screen and handle back button press
             VideoDetailScreen(onBackButtonClicked = {
+                // Navigate back to the main video screen
                 navController.navigate(NavigationItem.Video.route)
             }, it.arguments?.getString("videoId"))
         }
--- app/src/main/java/com/munbonecci/core/navigation/NavigationItem.kt
+++ app/src/main/java/com/munbonecci/core/navigation/NavigationItem.kt
@@ -1,27 +1,31 @@
 package com.munbonecci.core.navigation
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.ui.graphics.vector.ImageVector
-import com.munbonecci.videoplayer.R
-
-sealed class NavigationItem(var route: String, var icon: ImageVector, var title: Int) {
+/**
+ * Sealed class representing different navigation items in the application.
+ * Each object in the sealed class corresponds to a specific route in the navigation graph.
+ * @property route The route associated with the navigation item.
+ */
+sealed class NavigationItem(var route: String) {
+    /**
+     * Represents the main video listing screen.
+     */
     object Video :
         NavigationItem(
-            route = VIDEO_ROUTE,
-            icon = Icons.Default.Home,
-            title = R.string.video_category
+            route = VIDEO_ROUTE
         )
 
+    /**
+     * Represents the detailed view of a video.
+     * videoId The unique identifier of the video.
+     * videoName The name or title of the video.
+     */
     object VideoDetail :
         NavigationItem(
-            route = "$VIDEO_DETAIL_ROUTE/{videoId}/{videoName}",
-            icon = Icons.Default.Menu,
-            title = R.string.video_detail_category
+            route = "$VIDEO_DETAIL_ROUTE/{videoId}/{videoName}"
         )
 
     companion object {
+        // Constants representing route names
         const val VIDEO_ROUTE = "video"
         const val VIDEO_DETAIL_ROUTE = "video_detail"
     }
